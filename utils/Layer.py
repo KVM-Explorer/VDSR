73043d9e606497df0d97f7b18ff736463ec0a0ac
KVM <wfcyywh1225@126.com>
2022-05-19 16:45:54 +0800

    完成前向传播单元测试

--- utils/Layer.py
+++ utils/Layer.py
@@ -12,36 +12,37 @@ class Conv():
         self.padding = padding
         self.stride = stride
         self.kernel = torch.rand(*kernel)
-        self.bias = bias # Todo 每个通道对应bias
+        self.bias = torch.ones(kernel[0])*bias
         self.gradient_w = torch.zeros(* kernel)
         self.gradient_bias = torch.zeros(1)
 
     def forward(self,x:torch.tensor):
         '''
         多通道前向传播
-        :param x:  （batch,width,height,channels)
+        :param x:  （batch,channels,rows,cols)
         :return:
         '''
-        output = torch.zeros(*x.Size())
+        output = torch.zeros(*x.size())
         input = F.pad(x, [self.padding[0],self.padding[0],
                           self.padding[1],self.padding[1]],mode='constant',value=0) # padding 先后顺序不影响
 
-        rows = input.size()[1]
-        cols = input.size()[2]
-        kernel_row = self.kernel.Size()[2]
-        kernel_col = self.kernel.Size()[3]
-        for batch in range(x[0]):
-            for num in range(self.kernel[0]):
+        rows = input.size()[2]
+        cols = input.size()[3]
+        kernel_row = self.kernel.size()[2]
+        kernel_col = self.kernel.size()[3]
+
+        for batch in range(x.size()[0]):
+            for num in range(self.kernel.size()[0]):
                 for i in range(rows-kernel_row+1):
-                    for j in range(cols - kernel_col + 1):
-                        output[batch,i,j,:] = torch.sum(
+                    for j in range(cols - kernel_col+1):
+                        output[batch,:,i,j] = torch.sum(
                             # Kernel (num,channels,row,cols)
-                            input[batch,i+kernel_row,j+kernel_col,:] * self.kernel[num,:,:,:]
+                            input[batch,:,i:i+kernel_row,j:j+kernel_col] * self.kernel[num,:,:,:]
                             + self.bias[num]
                         )
         return output
 
-    def backward(self,loss):
+    # def backward(self,loss):
 
 
 class Relu():
