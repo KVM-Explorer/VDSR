b1fbc53b7073532e406284a57e4ae46733f53450
KVM <wfcyywh1225@126.com>
2022-05-19 11:55:38 +0800

    初步实现前向传播

--- utils/Layer.py
+++ utils/Layer.py
@@ -12,22 +12,36 @@ class Conv():
         self.padding = padding
         self.stride = stride
         self.kernel = torch.rand(*kernel)
-        self.bias = bias
+        self.bias = bias # Todo 每个通道对应bias
         self.gradient_w = torch.zeros(* kernel)
         self.gradient_bias = torch.zeros(1)
 
-    def forward(self,x):
+    def forward(self,x:torch.tensor):
         '''
         多通道前向传播
         :param x:  （batch,width,height,channels)
         :return:
         '''
-        input = F.pad(x, [1,1,1,1],
-              mode='constant',
-              value=0)
-        return input
+        output = torch.zeros(*x.Size())
+        input = F.pad(x, [self.padding[0],self.padding[0],
+                          self.padding[1],self.padding[1]],mode='constant',value=0) # padding 先后顺序不影响
 
-    # def backward(self,loss):
+        rows = input.size()[1]
+        cols = input.size()[2]
+        kernel_row = self.kernel.Size()[2]
+        kernel_col = self.kernel.Size()[3]
+        for batch in range(x[0]):
+            for num in range(self.kernel[0]):
+                for i in range(rows-kernel_row+1):
+                    for j in range(cols - kernel_col + 1):
+                        output[batch,i,j,:] = torch.sum(
+                            # Kernel (num,channels,row,cols)
+                            input[batch,i+kernel_row,j+kernel_col,:] * self.kernel[num,:,:,:]
+                            + self.bias[num]
+                        )
+        return output
+
+    def backward(self,loss):
 
 
 class Relu():
