6646018d86115e5fbbc01cf6673b356061cf1ebc
KVM <wfcyywh1225@126.com>
2022-05-25 12:56:51 +0800

    添加预处理插值缩放功能

--- utils/PreProcess.py
+++ utils/PreProcess.py
@@ -1,12 +1,20 @@
+import cv2
 import torch
 
 
-def normalize(x:torch.tensor):
+def normalize(x: torch.tensor, dsize):
     '''
     归一化和重新维度排序
+    :param dsize:
     :param x:
     :return:
     '''
-    x = x / 255.0
-    ret = torch.permute(x,[0,3,1,2])
-    return ret
+    ret = []
+    for i in range(x.size(0)):
+        img = x[i, :, :, :]
+        cv2.resize(img, dsize, interpolation=cv2.INTER_CUBIC)
+        ret.append(img)
+    ret = ret / 255.0
+    ret = torch.permute(ret, [0, 3, 1, 2])
+    print(f"- PreProcess finished")
+    return torch.tensor(ret, dtype=torch.float32)
