80dddaa6a5bb6e2686b7ce94ced14ec66ee68a27
KVM <wfcyywh1225@126.com>
2022-05-26 23:38:32 +0800

    添加解析模型输出函数

--- utils/PreProcess.py
+++ utils/PreProcess.py
@@ -2,9 +2,11 @@ import cv2
 import numpy
 import torch
 import logging
+
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -%(levelname)s - %(message)s')
 
-def normalize(x:numpy.ndarray, dsize):
+
+def normalize(x: numpy.ndarray, dsize):
     '''
     归一化和重新维度排序
     :param dsize:
@@ -22,3 +24,25 @@ def normalize(x:numpy.ndarray, dsize):
     ret = torch.permute(ret, [0, 3, 1, 2])
     logging.debug("PreProcess finished")
     return ret
+
+
+def test_normalize(image, dsize):
+    ret = []
+    img = cv2.resize(image, dsize, interpolation=cv2.INTER_CUBIC)
+    img = numpy.reshape(img, (img.shape[0], img.shape[1], 1))  # 灰度确
+    ret.append(img / 255.0)
+    ret = numpy.array(ret)
+    ret = torch.tensor(ret, dtype=torch.float32)
+    ret = torch.permute(ret, [0, 3, 1, 2])
+    return ret
+
+
+def reverse_data(x: torch.tensor):
+    '''
+    反转模型输出为单张图片
+    :param x:
+    :return:
+    '''
+    numpy_image = x.cpu().numpy().squeeze(0).transpose((1, 2, 0))
+    image_int = (numpy_image * 255.0).astype(int)
+    return image_int
