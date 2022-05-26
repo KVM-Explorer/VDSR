8a7f968fd383dcce6669ffcad9e584e00070278d
KVM <wfcyywh1225@126.com>
2022-05-26 23:38:51 +0800

    修复校验的错误

--- utils/BatchProcess.py
+++ utils/BatchProcess.py
@@ -1,5 +1,10 @@
+import torch
+
 import utils.Optimizer as Optimizer
 import utils.Net as Net
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -%(levelname)s - %(message)s')
 
 
 def train(data, model: Net.VDSR, epochs, optimizer: Optimizer.SGD, batch_size=16, ratio=0.7):
@@ -20,20 +25,25 @@ def train(data, model: Net.VDSR, epochs, optimizer: Optimizer.SGD, batch_size=16
             train_y_batch = train_y[j:min(len(train_y), j + batch_size)]
             ret = model.forward(train_x_batch)
             delta, loss = optimizer.solve(ret, train_y_batch)
+            logging.info(f"Loss:{loss}")
             model.backward(delta)
             model = optimizer.update(model)
 
         loss = valid(data=(valid_x, valid_y), model=model, optimizer=optimizer)
-        print(f"epoch:{i}/{epochs} loss:{loss}")
+        logging.info(f"epoch:{i}/{epochs} loss:{loss}")
+    return model
 
 def valid(data, model: Net.VDSR, optimizer: Optimizer.SGD):
+    logging.info("=================Start valid==================")
     x, y = data
     tot_num = len(x)
     total_loss = 0
+    batches, channels, rows, cols = x.size()
     for i in range(len(x)):
-        ret = model.forward(x[i, :, :, :])
+        tmp_x = torch.reshape(x[i, :, :, :], (1, channels, rows, cols))
+        ret = model.forward(tmp_x)
         _, loss = optimizer.solve(ret, y[i, :, :, :])
         total_loss += loss
 
     total_loss /= tot_num
-    return  total_loss
+    return total_loss
